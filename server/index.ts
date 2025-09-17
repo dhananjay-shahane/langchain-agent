9df6932f0a07f4ac18a34fa6cbe86b2f4380d47b
Dhananjay Shahane <143813959+DhananjayShahane@users.noreply.github.com>
2025-09-17 09:28:22 +0530

    update

--- server/index.ts
+++ server/index.ts
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    // reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
   });
